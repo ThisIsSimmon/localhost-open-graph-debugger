191873e37b520ef8e69fcd6b9e79c29410c78c8e
Simmon <thisissimmon@gmail.com>
2022-07-04 22:07:16 +0900

    :bookmark: Version 2

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,6 +1,26 @@
-chrome.action.onClicked.addListener((tab) => {
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		files: ['class-localhost-open-graph-debugger.js'],
-	});
+async function getCurrentTab() {
+	let queryOptions = { active: true, currentWindow: true };
+	let [tab] = await chrome.tabs.query(queryOptions);
+	return tab;
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	if ('start' === message) {
+		getCurrentTab().then((tab) => {
+			chrome.scripting.executeScript(
+				{
+					target: { tabId: tab.id },
+					files: ['app.js'],
+				},
+				(injectionResults) => {
+					setTimeout(() => {
+						chrome.runtime.sendMessage(injectionResults[0].result);
+					}, 1500);
+				}
+			);
+		});
+	}
+
+	sendResponse({});
+	return true;
 });
