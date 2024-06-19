b2311fad8e20e745e149843d4e15bf46b92db6be
Simmon <simmon@Simmon.local>
2024-06-19 15:41:17 +0900

    Add StyleX

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,6 @@
 import type { Preview } from '@storybook/react';
 import React from 'react';
+import '../src/index.css';
 
 const preview: Preview = {
 	parameters: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { crx } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import styleX from 'vite-plugin-stylex';
 import { manifest } from './manifest';
+
+const isStorybook = process.env.STORYBOOK === 'true';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), crx({ manifest })],
+	plugins: isStorybook ? [react(), styleX()] : [react(), styleX(), crx({ manifest })],
 	resolve: {
 		alias: {
 			'@': '/src',
