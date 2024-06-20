259198bcefa6cd0aedfab91a100c5d3929a500da
Simmon <simmon@Simmon.local>
2024-06-20 21:00:12 +0900

    Add SubmitButton component

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,9 @@
 import type { Preview } from '@storybook/react';
 import React from 'react';
 import '../src/index.css';
+import '@fontsource/mulish/400.css';
+import '@fontsource/mulish/700.css';
+import '@fontsource/mulish/900.css';
 
 const preview: Preview = {
 	parameters: {
