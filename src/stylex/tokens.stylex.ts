b402ffc1e200216fc8bd4e94dee518ce7ed0d01d
Simmon <simmon@Simmon.local>
2024-06-20 11:05:29 +0900

    refactor: Rename tokens.styles.ts to tokens.stylex.ts and add default values

--- src/stylex/tokens.stylex.ts
+++ src/stylex/tokens.stylex.ts
@@ -2,11 +2,13 @@ import * as stylex from '@stylexjs/stylex';
 
 export const colors = stylex.defineVars({
 	primary: {
+		default: '#6441C3',
 		normal: '#6441C3',
 		strong: '#462C8C',
 		weak: '#7C69B0',
 	},
 	warning: {
+		default: '#AE297F',
 		normal: '#AE297F',
 		strong: '#781C58',
 		weak: '#AE568F',
@@ -15,10 +17,12 @@ export const colors = stylex.defineVars({
 		default: '#fff',
 	},
 	background: {
+		default: 'rgb(255 255 255 / 0.1)',
 		button: 'rgb(255 255 255 / 0.1)',
 		toast: 'rgb(from #0F0330 r g b / 0.4)',
 	},
 	toastIcon: {
+		default: '#0FC970',
 		success: '#0FC970',
 		failed: '#FBAA08',
 	},
