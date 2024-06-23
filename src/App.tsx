d742ea23c4b005d0f61b8ae49e8427548207efc3
Simmon <simmon@Simmon-2.local>
2024-06-23 14:37:39 +0900

    WIP: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import { Background } from '@/components/Background';
 import { Description } from '@/components/Description';
 import { SubmitButton } from '@/components/SubmitButton';
 import { Title } from '@/components/Title/Title';
+import { Toast } from '@/components/Toast/Toast';
+import type { ToastAction, ToastState } from '@/types';
 import * as stylex from '@stylexjs/stylex';
+import { useReducer } from 'react';
 
 const styles = stylex.create({
 	container: {
@@ -18,16 +21,74 @@ const styles = stylex.create({
 	},
 });
 
+const initialState: ToastState = {
+	isOpen: false,
+	type: 'success',
+	title: '',
+	description: '',
+};
+
+const reducer = (state: ToastState, action: ToastAction): ToastState => {
+	switch (action.type) {
+		case 'open':
+			return { ...state, isOpen: true, ...action.payload };
+		case 'close':
+			return { ...state, isOpen: false };
+		default:
+			return state;
+	}
+};
+
 function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	const doSomething = () => {
+		console.log(document.body.innerHTML);
+	};
+
+	const getCurrentTab = async (): Promise<chrome.tabs.Tab> => {
+		const queryOptions = { active: true, currentWindow: true };
+		const [tab] = await chrome.tabs.query(queryOptions);
+		return tab;
+	};
+
+	const onSubmit = async () => {
+		const tab = await getCurrentTab();
+		if (tab.id === undefined) {
+			dispatch({
+				type: 'open',
+				payload: {
+					type: 'failed',
+					title: 'Failed',
+					description: 'Current tab not found',
+				},
+			});
+			return;
+		}
+
+		chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+			func: doSomething,
+		});
+	};
 	return (
 		<>
 			<Background />
+			<Toast
+				toast={{
+					isOpen: state.isOpen,
+					type: state.type,
+					title: state.title,
+					description: state.description,
+				}}
+				dispatch={dispatch}
+			/>
 			<div {...stylex.props(styles.container)}>
 				<div {...stylex.props(styles.guide)}>
 					<Title />
 					<Description />
 				</div>
-				<SubmitButton />
+				<SubmitButton onClick={onSubmit} />
 			</div>
 		</>
 	);
